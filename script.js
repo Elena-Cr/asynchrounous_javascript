ba066e06263d84f6b8f56b597607bf861e40b47f
Elena Cristescu <elenacristescu11@gmail.com>
2024-01-30 12:14:56 +0100

    make a call back function inside another callback function in order to display neighbouring countries

--- script.js
+++ script.js
@@ -3,18 +3,43 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+// const getCountryData = function (country) {
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+//   request.send();
+
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
+
+//     const html = `<article class="country">
+//   <img class="country__img" src="${data.flags.png}" />
+//   <div class="country__data">
+//     <h3 class="country__name">${data.name.common}</h3>
+//     <h4 class="country__region">${data.region}</h4>
+//     <p class="country__row"><span>👫</span>${(
+//       +data.population / 1000000
+//     ).toFixed(1)}</p>
+//     <p class="country__row"><span>🗣️</span>${
+//       Object.values(data.languages)[0]
+//     }</p>
+//     <p class="country__row"><span>💰</span>${
+//       Object.values(data.currencies)[0].name
+//     }</p>
+//   </div>
+// </article>`;
+//     countriesContainer.insertAdjacentHTML('beforeend', html);
+//     countriesContainer.style.opacity = 1;
+//   });
+// };
+
+// getCountryData('portugal');
+// getCountryData('denmark');
+
 ///////////////////////////////////////
-//https://countries-api-836d.onrender.com/countries/
-const getCountryData = function (country) {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-  request.send();
-
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
-
-    const html = `<article class="country">
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
   <img class="country__img" src="${data.flags.png}" />
   <div class="country__data">
     <h3 class="country__name">${data.name.common}</h3>
@@ -30,10 +55,38 @@ const getCountryData = function (country) {
     }</p>
   </div>
 </article>`;
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+//https://countries-api-836d.onrender.com/countries/
+const getCountryAndNeighbour = function (country) {
+  //AJAX call first country
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    //Render the first country
+    renderCountry(data);
+    //Get neighbour country
+    const neighbour = data.borders?.[0];
+    if (!neighbour) return;
+
+    //AJAX call for neighbour country
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const [data2] = JSON.parse(this.responseText);
+      console.log(data2);
+      //Render the first country
+      renderCountry(data2, 'neighbour');
+    });
   });
 };
 
-getCountryData('portugal');
-getCountryData('denmark');
+getCountryAndNeighbour('denmark');
