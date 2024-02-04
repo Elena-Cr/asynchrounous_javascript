db11ee4a7d09c49508551fab25b3258401783c66
Elena Cristescu <elenacristescu11@gmail.com>
2024-02-04 13:29:09 +0100

    running promises in paralel with Promise.all

--- script.js
+++ script.js
@@ -138,19 +138,18 @@ const countriesContainer = document.querySelector('.countries');
 // //   getCountryData('portugal');
 // // });
 
-// const getJSON = function (url1, errorMsg = 'Something went wrong') {
-//   fetch(url1).then(response => {
-//     if (!response.ok) {
-//       throw new Error(`${errorMsg} (${response.status})`);
-//     }
-
-//     return response.json();
-//   });
-// };
-// const renderError = function (msg) {
-//   countriesContainer.insertAdjacentText('beforeend', msg);
-//   countriesContainer.style.opacity = 1;
-// };
+const getJSON = function (url1, errorMsg = 'Something went wrong') {
+  return fetch(url1).then(response => {
+    if (!response.ok) {
+      throw new Error(`${errorMsg} (${response.status})`);
+    }
+    return response.json();
+  });
+};
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  countriesContainer.style.opacity = 1;
+};
 // const request = fetch('https://restcountries.com/v3.1/name/portugal');
 // console.log(request);
 
@@ -279,3 +278,22 @@ console.log('1: Will get location ');
 // } catch (err) {
 //   alert(err.message);
 // }
+
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+    // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+    // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c3}`),
+    ]);
+    console.log(data.map(d => d[0].capital));
+    // console.log([data1.capital, data2.capital, data3.capital]);
+  } catch (err) {
+    console.error(err);
+  }
+};
+get3Countries('portugal', 'canada', 'tanzania');
